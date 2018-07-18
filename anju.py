c323c2d552ca6eb5ff7b822cb38e12424c26e542
jiju123 <jijusebastain2@gmail.com>
2018-07-18 17:55:33 +0530

    first commit

---
+++ anju.py
@@ -0,0 +1,20 @@
+def add():
+ a=int(input("ENTER YOUR FIRST NUMBER:"))
+ b=int(input("ENTER YOUR SECOND NUMBER:"))
+ c=a+b
+ print(c)
+def sub():
+ a=int(input("ENTER YOUR FIRST NUMBER:"))
+ b=int(input("ENTER YOUR SECOND NUMBER:"))
+ c=a-b
+ print(c)
+def mul():
+ a=int(input("ENTER YOUR FIRST NUMBER:"))
+ b=int(input("ENTER YOUR SECOND NUMBER:"))
+ c=a*b
+ print(c)
+def div():
+ a=int(input("ENTER YOUR FIRST NUMBER:"))
+ b=int(input("ENTER YOUR SECOND NUMBER:"))
+ c=a//b
+ print(c)
